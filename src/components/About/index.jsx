a8dc40fd0cb0317ca5b3d45a92329be35722e803
Yousef Rizik <56390606+92yo@users.noreply.github.com>
2020-05-05 22:19:48 +0300

    Update index.jsx

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -9,19 +9,18 @@ export default function About() {
       <div id="about" className={'mainContainer flow-text'}>
         <blockquote>
           <h2>
-          Mission-driven full stack developer with a 
-          passion for thoughtful UI design, 
-          teamwork, and programming.
+          A mission-driven Full Stack Developer with a 
+          passion for UI design,teamwork and programming.
           </h2>
         </blockquote>
             <p>
                 I've always sought out opportunities and challenges that are meaningful to me. 
-                Although my professional path has taken many twists and turns - from Volunteering at a young age, to leaving everything behind after graduation at the age of 17 to live abroad and study in the United States for 5 years,
-                to studying at Haifa Industrial Engineering, to eventually finding my true calling in programming -
+                Although my professional path has taken many twists and turns - from volunteering at a young age to leaving everything behind after graduation at the age of 17 to live abroad and study in the United States for 5 years,
+                to studying Industrial Engineering in Haifa, to eventually finding my true calling in programming -
                 I've never stopped engaging my passion to help others and solve problems.
             </p>
             <p>
-                As a Full Stack Developer, i enjoy using my obsessive attention to detail, my unequivocal love for making things, and my mission-driven work ethic to literally change the world.
+                As a Full Stack Developer, I enjoy using my meticulous attention to detail, my unequivocal love for making things, and my mission-driven work ethic to literally change the world.
                 That's why I'm excited to make a big impact at a high growth company.
             </p>
             <CopyRight/>
